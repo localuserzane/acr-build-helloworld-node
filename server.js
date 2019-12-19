fd0509b7bcd5c645826cc4d08c32df86d6d35afd
localuserzane <zanen@inbox.lv>
2019-12-19 17:15:02 +0200

    testing, text 3rd line

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const port = 80
 
 const server = http.createServer((request, response) => {
   response.writeHead(200, {'Content-Type': 'text/plain'})
-  response.write("Hello From The Other Side!\nI must've called a thousand times.\n")
+  response.write("Hello From The Other Side!\nI must've called a thousand times.\nTo Tell You...")
   response.end('Version: ' + process.env.NODE_VERSION + '\n')
 })
 
